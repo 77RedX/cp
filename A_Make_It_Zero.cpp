22451bd558b3557b2a57a7ce08e7115d177fd37a
77RedX <akshaygautam4450@gmail.com>
2025-11-29 01:46:44 +0530

    Solved

--- A_Make_It_Zero.cpp
+++ A_Make_It_Zero.cpp
@@ -8,6 +8,20 @@ int main(){
         cin>>n;
         vector<int> a(n);
         for(int i=0;i<n;i++) cin>>a[i];
-        
+        if(n%2){
+            cout<<4<<endl;
+            cout<<1<<" "<<n-1<<endl;
+            cout<<1<<" "<<n-1<<endl;
+            cout<<2<<" "<<n<<endl;
+            cout<<2<<" "<<n<<endl;
+        }
+        /*agr even n hai, jo bhi number bane pehle xor loop pe
+        next time sab ek doosre ko cancel krdega
+        ez*/
+        else{
+            cout<<"2\n";
+            cout<<1<<" "<<n<<endl;
+            cout<<1<<" "<<n<<endl;
+        }
     }
 }
