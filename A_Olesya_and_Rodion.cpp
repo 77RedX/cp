51f7b0d109119eac420140be095d735e2fa6a13b
77RedX <akshaygautam4450@gmail.com>
2025-11-29 02:52:01 +0530

    Solved

--- A_Olesya_and_Rodion.cpp
+++ A_Olesya_and_Rodion.cpp
@@ -1,38 +1,33 @@
 #include <bits/stdc++.h>
+#define int long long
+#define MXI 1000000007LL
+#define vi vector<int>
+#define cy cout << "YES" << endl
+#define cn cout << "NO" << endl
 using namespace std;
-int main(){
-    int n,t;
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    string t;
     cin>>n>>t;
-    string s;
-    if(n==1 && t==10){
-        cout<<"-1";
-        return 0;
-    }
-    else if(n==1 && t<10){
-        cout<<t;
-        return 0;
-    }
-    for(int i=0;i<=n-1;i++){ // gendu logic
-        if(i==0 && n>=3){
-            s+='1';
+    string sol="";
+    //ez koshan?
+    if(t=="10"){
+        if(n==1){
+            cout<<-1<<endl;
+            return 0;
         }
-        else if(i==n-2 || n==2){
-            if(t==2 || t==4 || t==8 || t==6){
-                s+="24";
-                break;
-            }
-            else if(t==3 || t==9) {
-                s+="18";
-                break;
-            }
-            else{
-                s+="35";
-                break;
-            }
+        for(int i=0;i<n-1;i++){
+            sol+="1";
         }
-        else{
-            s+='0';
+        sol+="0";
+    }
+    else{
+        for(int i=0;i<n;i++){
+            sol+=t;
         }
     }
-    cout<<s;
+    cout<<sol;
+    return 0;
 }
